cf895a7ac245d7a90571bdd2bf97c0df94a6fddd
emna <emna.gaidii@gmail.com>
2024-04-12 00:24:59 +0100

    first commit

---
+++ wordcount-spark/src/main/java/spark/batch/tp21/WordCountTask.java
@@ -0,0 +1,36 @@
+package spark.batch.tp21;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import com.google.common.base.Preconditions;
+
+public class WordCountTask {
+      private static final Logger LOGGER = LoggerFactory.getLogger(WordCountTask.class);
+
+      public static void main(String[] args) {
+          Preconditions.checkArgument(args.length > 1, "Please provide the path of input file and output dir as parameters.");
+          new WordCountTask().run(args[0], args[1]);
+      }
+
+      public void run(String inputFilePath, String outputDir) {
+          //String master = "local[*]";
+          SparkConf conf = new SparkConf()
+                  .setAppName(WordCountTask.class.getName());
+                  //.setMaster(master);
+          JavaSparkContext sc = new JavaSparkContext(conf);
+
+          JavaRDD<String> textFile = sc.textFile(inputFilePath);
+          JavaPairRDD<String, Integer> counts = textFile
+                  .flatMap(s -> Arrays.asList(s.split("\t")).iterator())
+                  .mapToPair(word -> new Tuple2<>(word, 1))
+                  .reduceByKey((a, b) -> a + b);
+          counts.saveAsTextFile(outputDir);
+      }
+  }
+
